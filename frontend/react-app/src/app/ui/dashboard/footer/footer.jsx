f53d14dbccf6fa8cfb6bd0f0dbd8fcb38fd5a0d3
Shaoxiao Xu <shaoxixu@tesla.com>
2023-12-01 14:43:15 +0800

    front source code updation

--- frontend/react-app/src/app/ui/dashboard/footer/footer.jsx
+++ frontend/react-app/src/app/ui/dashboard/footer/footer.jsx
@@ -3,7 +3,7 @@ import styles from "./footer.module.css";
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>Lama Dev</div>
+      <div className={styles.logo}>VDNA Team.</div>
       <div className={styles.text}>© All rights reserved.</div>
     </div>
   );
